06b6553acb4acb8598c3068cb848291c53debe76
Aarushi <aarushidhanuka@hotmail.com>
2023-07-05 17:40:28 +0530

    Updated on 5 july 2023

--- version1.c
+++ version1.c
@@ -3,6 +3,82 @@
 #include <math.h>
 #include <complex.h>
 
+float complex* destroy_op; 
+float complex* identity_op;
+float complex* tensor_result;
+float complex* dagres;
+float complex* matrixmul;
+float complex** c_ops;
+float complex* dag(float complex* op, int size, float complex* dag_res);
+float complex* matrix_mul(float complex* matrix1, int size1, float complex* matrix2, int size2, float complex* result);
+float complex* tensor_product(float complex* op1, int size1, float complex* op2, int size2, float complex* result);
+float complex* liovillian(float complex* H, int size1, float complex** c_ops, int size2, float complex* result); 
+
+// Define the liovillian operator
+float complex* liovillian(float complex* H, int size1, float complex** c_ops, int size2, float complex* result){
+
+	int totalsize = size1*size1;
+	float complex* cop1 = (float complex*)malloc(size2 * size2* sizeof(float complex));
+	float complex* cop2 = (float complex*)malloc(size2 * size2* sizeof(float complex));
+
+	
+	// Define the identity operator
+	float complex identity[size1*size1];
+	for(int i=0;i<size1;i++){
+		for(int j=0;j<size1;j++){
+			if(i==j)
+			{
+				identity[i*size1 + j] = 1;
+			}
+			else
+				identity[i*size1 + j] =0;
+		}
+	}
+	
+	for(int i=0;i<size2;i++)
+	{
+		for(int j=0;j<size2;j++){
+			cop1[i*size2 + j] = c_ops[0][i*size2 + j];
+		}	
+	}
+	
+	for(int i=0;i<size2;i++)
+	{
+		for(int j=0;j<size2;j++){
+			cop2[i*size2 + j] = c_ops[1][i*size2 + j];
+		}	
+	}
+
+	// Define the tensor products
+	float complex* h_id= tensor_product(H, totalsize, identity, totalsize, tensor_result);
+	float complex* id_h= tensor_product(identity, totalsize, H, totalsize, tensor_result);
+	float complex* c1_c2 = tensor_product(cop1, totalsize, cop2, totalsize, tensor_result);
+	
+	// Define the conjugate transpose
+	float complex* c2dag = dag(cop1,size2,dagres);
+	
+	// Define the matrix multiplication
+	float complex* c2dagc1 = matrix_mul(c2dag,size2,cop1,size2,matrixmul);
+	
+	// Define the tensor products for 1_c2dagc1, c2dagc1_1
+	float complex* id_c2dagc1 = tensor_product(identity, totalsize, c2dagc1, totalsize, tensor_result);
+	float complex* c2dagc1_id = tensor_product(c2dagc1, totalsize, identity, totalsize, tensor_result);
+	
+	// Define the term1 for L
+	float complex liouvillian_op[totalsize*totalsize];
+
+	for(int i=0;i<totalsize;i++){
+		for(int j=0;j<totalsize;j++){
+			result[i*totalsize + j] = cimag(-1)*(h_id[i*totalsize + j]-id_h[i*totalsize + j]) + 0.5*5*(2*c2dagc1[i*totalsize + j]-id_c2dagc1[i*totalsize + j]-c2dagc1_id[i*totalsize + j]);	
+		}
+	}
+	
+	free(cop1);
+	free(cop2);
+	
+	return result;
+}
+
 
 // Define the conjugate transpose function
 float complex* dag(float complex* op, int size, float complex* dag_res){
@@ -20,7 +96,7 @@ float complex* dag(float complex* op, int size, float complex* dag_res){
 // Define the matrix multiplication
 
 float complex* matrix_mul(float complex* matrix1, int size1, float complex* matrix2, int size2, float complex* result){
-	 for (int i = 0; i < size1; i++) {
+     for (int i = 0; i < size1; i++) {
         for (int j = 0; j < size2; j++) {
             result[i*size1+j] = 0.0;
             for (int k = 0; k < size1; k++) {
@@ -75,7 +151,6 @@ int main() {
     float complex a_dag[size*size];
     float complex b_dag[size*size];
     float complex H[size*size];
-    float complex c_ops[size*size];
     int k=0;
     int delta = 5;
     int gamma=1;
@@ -90,6 +165,17 @@ int main() {
     float complex* tensor_result = (float complex*)malloc(cutoff * cutoff * cutoff * cutoff * sizeof(float complex));
     float complex* dagres = (float complex*)malloc(cutoff * cutoff * cutoff * cutoff * sizeof(float complex));
     float complex* matrixmul = (float complex*)malloc(cutoff * cutoff * cutoff * cutoff * sizeof(float complex));
+    float complex* li_result = (float complex*)malloc(size * size * size * size * sizeof(float complex));
+    
+    //To create a list of arrays in c_ops
+    int sizelist = 2;
+    int size_arraylist = size*size;
+    float complex** c_ops = (float complex**)malloc(sizelist * sizeof(float complex*));
+	for (int i = 0; i < sizelist; i++) {
+	    c_ops[i] = (float complex*)malloc(size_arraylist * sizeof(float complex));
+	}
+
+
    
     
     // Generate destroy operator
@@ -157,26 +243,24 @@ int main() {
    
    
    // Build the c_ops list
+   for (int j = 0; j < size; j++) {
+       for (int k = 0; k < size; k++)
+        	{
+        		c_ops[0][j*size + k] = sqrt(gamma)*a[j*size + k];  	
+        	}
+    }
+   
    for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-         	c_ops[i*size + j] = sqrt(gamma)*a[i*size + j];   
+         	c_ops[1][i*size + j] = sqrt(gamma)*b[i*size + j]; 
     }
    }
    
-   int t=0,l=0;
-   for (int i = size; i < (2*size); i++) {
-        for (int j = size; j < (2*size); j++) {
-         	c_ops[i*size + j] = sqrt(gamma)*b[t*size + l];
-         	 l++;   
-    }
-    t++;
-   }
-   
-   
+   float complex* li_op = liovillian(H,size,c_ops,size,li_result);
    
    // Print whatever you want (timepass)😵️
     
-    printf("Destroy op: ");
+    /*printf("Destroy op: ");
     for (int i = 0; i < cutoff; i++) {
         for (int j = 0; j < cutoff; j++) {
             printf("%.2f + %.2fi ", creal(destroy_op[i*cutoff + j]),cimag(destroy_op[i*cutoff + j]));
@@ -205,14 +289,30 @@ int main() {
         		printf("%.2f + %.2fi ", creal(H[i*cutoff + j]),cimag(H[i*cutoff + j]));	
         	}
       	}
-  }
+  }*/
   
+  
+  /*printf("Value of c_ops");
+   for (int i = 0; i < sizelist; i++) {
+        for (int j = 0; j < size_arraylist; j++) {
+        	if(creal(c_ops[i][j])!=0)
+        	{
+        		printf("%.2f + %.2fi ", creal(c_ops[i][j]),cimag(c_ops[i][j]));	
+        	}
+      	}
+  }*/
+  
+  printf("%.2f + %.2fi ", creal(c_ops[0][45]),cimag(c_ops[0][45]));
   
     
     // Free allocated memory
     free(destroy_op);
     free(identity_op);
     free(tensor_result);
+    free(dagres);
+    free(matrixmul);
+    free(li_result);
+    free(c_ops);
     
     return 0;
 }
