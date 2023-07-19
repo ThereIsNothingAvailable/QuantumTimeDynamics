0dfa2f7285d616a79e899cc5207bff8975cb694b
Aarushi <aarushidhanuka@hotmail.com>
2023-07-19 15:17:49 +0530

    Updated calculation of eigenvalues and eigenvectors

--- factorization.c
+++ factorization.c
@@ -1,115 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <complex.h>
 #include<string.h>
 #include <time.h>
 #include "f2c.h"
-#include "clapack.h"
 #include "blaswrap.h"
+#include "clapack.h"
 
-#define MAX_SIZE 100
+
+/* Complex datatype */
+struct _fcomplex { float re, im; };
+typedef struct _fcomplex fcomplex;
+
+fcomplex random_complex() {
+    fcomplex z;
+    z.re = (float)rand()/ RAND_MAX; // Random real part between 0 and 1
+    z.im = (float)rand() /RAND_MAX; // Random imaginary part between 0 and 1
+    return z;
+}
+
+/* Parameters */
+#define N 100
+#define LDA N
+#define LDVL N
+#define LDVR N
 
 int main() {
-    int n = 250;
+    int m = 250;
     int k = 100; // Dimensions of the original matrix
-    double complex h[251*250]; // Original matrix (flattened)
-    double complex vals_eff[100]; // Array to store eigenvalues
-    double complex vecs_eff[100 * 100]; // Array to store eigenvectors
-    int sum =0;
+    fcomplex h[(m+1)*m]; // Original matrix (flattened)
+    //double complex vals_eff[100]; // Array to store eigenvalues
+    //double complex vecs_eff[100 * 100]; // Array to store eigenvectors
 
     // Fill the original matrix 'h' with your data
-    // Fill the original matrix 'h' with your data
-    for(int i=0;i<(n+1);i++)
-    {
-    	for(int j=0;j<n;j++)
-    	{
-    		h[i*n + j] = (double)rand() / RAND_MAX;	
-    	}
+    for (int i = 0; i < (m+1); i++) {
+        for (int j = 0; j < m; j++) {
+            h[i*m+j] = random_complex();
+        }
     }
+   
     
-    for(int i=0;i<(n+1);i++)
+    // Printing the upper hessenburg matrix
+    /*printf("Upper hessenburg matrix is : ");
+    for( int i=0;i<(m+1);i++)
     {
-    	for(int j=0;j<n;j++)
+    	for(int j =0;j<m;j++)
     	{
-    		sum = sum + h[i*n + j];	
+    		printf("for the index %d : %.2f + %.2f", (i*m+j),h[i*m+j].re,h[i*m+j].im);
     	}
-    }
-
-    // Normalize the vector by dividing each element by the sum
-    for(int i=0;i<(n+1);i++)
-    {
-    	for(int j=0;j<n;j++)
-    	{
-    		h[i*n + j] = sum/h[i*n + j];	
-    	}
-    }
-
-    // Compute the effective eigenvalues and eigenvectors
-    int info;
-    int lda = k; // Leading dimension of the sub-matrix
-
+    }*/
+    
     // Allocate memory for sub-matrix
-    double complex submatrix[MAX_SIZE * MAX_SIZE];
+    fcomplex submatrix[k * k];
 
     // Copy the sub-matrix from the original matrix 'h'
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < k; j++) {
-            submatrix[i * k + j] = h[i * (n + 1) + j];
+            submatrix[i * k + j] = h[i *m + j];
         }
     }
+    
 
-    char jobvl = 'N'; // Do not compute left eigenvectors
-    char jobvr = 'V'; // Compute right eigenvectors
+    // Variables to store eigenvalues and eigenvectors
 
-    // Temporary workspace and sizes
-    double complex work_query;
-    doublereal rwork;
-    int lwork = -1;
-    int ldvr =-1;
-    int ldvl = -1;
+    // LAPACK function to compute eigenvalues and eigenvectors
+    char jobvl = 'N'; // Compute left eigenvectors (N for no, V for yes)
+    char jobvr = 'V'; // Compute right eigenvectors (N for no, V for yes)
+   
+    int n = N, lda = LDA, ldvl = LDVL, ldvr = LDVR, info, lwork;
+    fcomplex wkopt;
+    fcomplex* work;
+    float rwork[2*N];
+    fcomplex w[N], vl[LDVL*N], vr[LDVR*N];
 
-    // Query the workspace size;
-    zgeev_(&jobvl, &jobvr, &k, submatrix, &lda, vals_eff, vecs_eff, &ldvl, vecs_eff, &ldvr, &work_query, &lwork, &rwork, &info);
+    lwork = -1;
+    cgeev_( "Vectors", "Vectors", &n, submatrix, &lda, w, vl, &ldvl, vr, &ldvr, &wkopt, &lwork, rwork, &info );
 
-
-    if (info != 0) {
-        printf("Error: zgeev_ failed with error code %d\n", info);
-        return info;
+    lwork = (int)wkopt.re;
+    work = (fcomplex*)malloc( lwork*sizeof(fcomplex) );
+    /* Solve eigenproblem */
+    cgeev_( "Vectors", "Vectors", &n, submatrix, &lda, w, vl, &ldvl, vr, &ldvr, work, &lwork, rwork, &info );
+    
+    /* Check for convergence */
+    if( info > 0 ) {
+        printf( "The algorithm failed to compute eigenvalues.\n" );
+        exit( 1 );
     }
 
-    // Allocate memory for workspace
-    lwork = (int)creal(work_query);
-    double complex* work = (double complex*)malloc(lwork * sizeof(double complex));
-
-    // Compute eigenvalues and eigenvectors with allocated workspace
-      zgeev_(&jobvl, &jobvr, &k, submatrix, &lda, vals_eff, vecs_eff, &ldvl, vecs_eff, &ldvr, &work_query, &lwork, &rwork, &info);
-
-
-    if (info != 0) {
-        printf("Error: zgeev_ failed with error code %d\n", info);
-        free(work);
-        return info;
-    }
-
-    // Print the effective eigenvalues
-    printf("Effective Eigenvalues:\n");
+    // Print the eigenvalues and eigenvectors
+    printf("\nEigenvalues:\n");
     for (int i = 0; i < k; i++) {
-        printf("%f + %fi\n", creal(vals_eff[i]), cimag(vals_eff[i]));
-    }
-    printf("\n");
-
-    // Print the effective eigenvectors
-    printf("Effective Eigenvectors:\n");
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < k; j++) {
-            printf("%f + %fi\t", creal(vecs_eff[i * k + j]), cimag(vecs_eff[i * k + j]));
-        }
-        printf("\n");
+        printf("%.14f + %.14fi\n", w[i].re, w[i].im);
     }
 
-    // Free allocated memory
-    free(work);
+    // Print the left eigenvectors (if computed)
+	if (jobvl == 'V') {
+	    printf("\nLeft Eigenvectors:\n");
+	    for (int i = 0; i < k; i++) {
+		for (int j = 0; j <k; j++) {
+		    printf("%.2f + %.2fi\t", vl[i*k + j].re, vl[i*k + j].im);
+		}
+		printf("\n");
+	    }
+	}
 
+	// Print the right eigenvectors (if computed)
+	if (jobvr == 'V') {
+	    printf("\nRight Eigenvectors:\n");
+	    for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k; j++) {
+		    printf("%.2f + %.2fi\t", vr[i*k+j].re, vr[i*k+j].im);
+		}
+		printf("\n");
+	    }
+	}
+   
     return 0;
 }
+
